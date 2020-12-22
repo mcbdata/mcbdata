7f6e4a1493df89dea608a393282f6976a128a4fd
Bryan Benson <bryanbenson83@gmail.com>
2020-12-21 21:36:51 -0500

    Start work on WP theme

---
+++ mcbdata_theme/footer.php
@@ -0,0 +1,87 @@
+   </section>
+
+		
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2">Contact Us</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">77 Water Street, 8th Floor, New York, NY 10005</span></li>
+	                <li><a href="tel:646-893-1663"><span class="icon icon-phone"></span><span class="text">+1 (646) 893-1663</span></a></li>
+	                <li><a href="mailto:info@bernsteindata.com"><span class="icon icon-envelope"></span><span class="text">info@bernsteindata.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-2">
+            <div class="ftco-footer-widget mb-5 ml-md-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="index.html"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+                <li><a href="what-we-do.html"><span class="ion-ios-arrow-round-forward mr-2"></span>What We Do</a></li>
+                <li><a href="why-us.html"><span class="ion-ios-arrow-round-forward mr-2"></span>Why Us</a></li>
+                <li><a href="what-we-provide.html"><span class="ion-ios-arrow-round-forward mr-2"></span>What We Provide</a></li>
+                <li><a href="perspectives.html"><span class="ion-ios-arrow-round-forward mr-2"></span>Perspectives</a></li>
+                <li><a href="contact.html"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2">Subscribe to Our Newsletter</h2>
+               <form action="https://formspree.io/info@bernsteindata.com" method="POST" class="subscribe-form"/>
+                <div class="form-group">
+                  <input type="text" name="email" id="email" class="form-control mb-2 text-center" placeholder="Enter email address">
+                  <input type="submit" value="Subscribe" class="form-control submit px-3">
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-2">
+            <div class="ftco-footer-widget mb-5">
+            	<h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
+            	<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                <li class="ftco-animate"><a href="https://twitter.com/BernsteinData" target="_blank"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.linkedin.com/company/mc-bernstein-data" target="_blank"><span class="icon-linkedin"></span></a></li>
+              </ul>
+            </div>
+   		  </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy; <script>document.write(new Date().getFullYear());</script> MC Bernstein Data. All rights reserved | Designed by <a href="https://moonlitecreative.com" target="_blank">Moonlite Creative</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+  <?php wp_footer();?>
+  </body>
+</html>
---
+++ mcbdata_theme/header.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>MC Bernstein Data</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=EB+Garamond&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <link rel="shortcut icon" href="images/mcb-favicon.png">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158126022-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-158126022-1');
+    </script>
+    <?php wp_head();?>
+  </head>
+  <body>
+	  <div class="bg-top navbar-light">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-center align-items-stretch">
+    			<div class="col-md-4 d-flex align-items-center py-4">
+    				<a href="index.html"><img class="navbar-brand" href="index.html" src="images/mcb2.png"></a>
+    			</div>
+	    		<div class="col-lg-8 d-block">
+		    		<div class="row d-flex">
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4 no-mobile">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+					    	<div class="text">
+					    		<span>Email</span>
+						    	<span><a href="mailto:info@bernsteindata.com">info@bernsteindata.com</a></span>
+						    </div>
+					    </div>
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4 no-mobile">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <div class="text">
+						    	<span>Call</span>
+						    	<span><a href="tel:646-893-1663">Call Us: +1 (646) 893-1663</a></span>
+						    </div>
+					    </div>
+					    <div class="col-md topper d-flex align-items-center justify-content-end no-mobile">
+					    	<p class="mb-0 d-block">
+					    		<a href="" class="btn py-2 px-3 btn-primary" data-toggle="modal" data-target="#modalSubscriptionForm">
+					    			<span>Subscribe to Our Newsletter</span>
+					    		</a>
+					    	</p>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </div>
+
+   <div class="modal fade" id="modalSubscriptionForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header text-center">
+	        <h4 class="modal-title w-100 font-weight-bold">Subscribe to Our Newsletter</h4>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="ftco-footer ftco-footer-widget">
+	               <form action="https://formspree.io/info@bernsteindata.com" method="POST" class="subscribe-form"/>
+	                <div class="form-group">
+	                  <input type="text" name="email" id="email" class="form-control mb-2 text-center" placeholder="Enter email address">
+	                  <input type="submit" value="Subscribe" class="form-control submit px-3">
+	                </div>
+	              </form>
+	              <h6>MC Bernstein Data is committed to protecting and respecting your privacy, and we’ll only use your personal information to provide our content that may be of interest to you.</h6>	
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav">
+	        	<li class="nav-item active"><a href="index.html" class="nav-link pl-0">Home</a></li>
+	        	<li class="nav-item"><a href="what-we-do.html" class="nav-link">What We Do</a></li>
+	        	<li class="nav-item"><a href="why-us.html" class="nav-link">Why Us</a></li>
+	        	<li class="nav-item"><a href="what-we-provide.html" class="nav-link">What We Provide</a></li>
+	        	<li class="nav-item"><a href="perspectives.html" class="nav-link">Perspectives</a></li>
+	            <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    
+    <section class="home-slider owl-carousel">
